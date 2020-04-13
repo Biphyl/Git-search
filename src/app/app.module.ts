b95baf508cffae7e3214a208bdfc226a214d511b
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-04-13 19:16:18 +0300

    generated user-repository component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { GithubFormComponent } from './github-form/github-form.component';
 import { ProfileComponent } from './profile/profile.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { UserRepositoryComponent } from './user-repository/user-repository.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GithubFormComponent,
     ProfileComponent,
-    UserProfileComponent
+    UserProfileComponent,
+    UserRepositoryComponent
   ],
   imports: [
     BrowserModule,
